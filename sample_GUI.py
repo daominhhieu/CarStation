72d6deed4cf33f999b843b53b333ab6fb3b4f8af
toiluonlahieu <toiluonlahieu@gmail.com>
2020-11-23 23:35:41 +0700

    Create draft outline for GUI

--- sample_GUI.py
+++ sample_GUI.py
@@ -6,19 +6,27 @@ class car_station_app(Tk):
         # __init__ function for class Tk 
         Tk.__init__(self, *args, **kwargs) 
         
-        # creating a container 
-        container = Frame(self)   
-        container.pack(side = "top", fill = "both", expand = True)  
+        self.columnconfigure(0, weight=45)
+        self.columnconfigure(1, weight=55)
+        self.rowconfigure(0, weight=10)
+        self.rowconfigure(1, weight=1)
+        self.attributes("-fullscreen", True)
 
-        container.grid_rowconfigure(0, weight = 1) 
-        container.grid_columnconfigure(0, weight = 1) 
+        self.M_section = MenuSection(self, self)
+        self.M_section.grid(column = 0, row = 0, sticky = 'nsew')
+
+        # creating a container 
+        container = Frame(self)
+        
+        container.grid(column = 1, row =0, sticky = 'nsew')
 
         # initializing frames to an empty array 
         self.frames = {}   
+        
 
         # iterating through a tuple consisting 
         # of the different page layouts 
-        for F in (StartPage, Page1, Page2): 
+        for F in (GeneralPage, DriverPage, VehicleInormationPage, TransactionHistoryPage, DebugPage): 
 
             frame = F(container, self) 
 
@@ -27,122 +35,77 @@ class car_station_app(Tk):
             # for loop 
             self.frames[F] = frame  
 
-            frame.grid(row = 0, column = 0, sticky ="nsew") 
+            frame.grid(row = 0, column = 1, sticky ="nsew") 
 
-        self.show_frame(StartPage) 
+        self.show_frame(GeneralPage)
 
     # to display the current frame passed as 
     # parameter 
     def show_frame(self, cont): 
         frame = self.frames[cont] 
         frame.tkraise() 
-class StartPage(Frame): 
-    def __init__(self, parent, controller):  
-        Frame.__init__(self, parent) 
-          
-        # label of frame Layout 2 
-        label = Label(self, text ="Startpage") 
-          
-        # putting the grid in its place by using 
-        # grid 
-        label.grid(row = 0, column = 4, padx = 10, pady = 10)  
-   
-        button1 = Button(self, text ="Page 1", 
-        command = lambda : controller.show_frame(Page1)) 
-      
-        # putting the button in its place by 
-        # using grid 
-        button1.grid(row = 1, column = 1, padx = 10, pady = 10) 
-   
-        ## button to show frame 2 with text layout2 
-        button2 = Button(self, text ="Page 2", 
-        command = lambda : controller.show_frame(Page2)) 
-      
-        # putting the button in its place by 
-        # using grid 
-        button2.grid(row = 2, column = 1, padx = 10, pady = 10) 
-   
-           
-   
-   
-# second window frame page1  
-class Page1(Frame): 
-      
-    def __init__(self, parent, controller): 
-          
-        Frame.__init__(self, parent) 
-        label = Label(self, text ="Page 1") 
+
+class GeneralPage(Frame):
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        label = Label(self, text ="Genral Page")
         label.grid(row = 0, column = 4, padx = 10, pady = 10) 
-   
-        # button to show frame 2 with text 
-        # layout2 
-        button1 = Button(self, text ="StartPage", 
-                            command = lambda : controller.show_frame(StartPage)) 
-      
-        # putting the button in its place  
-        # by using grid 
-        button1.grid(row = 1, column = 1, padx = 10, pady = 10) 
-   
-        # button to show frame 2 with text 
-        # layout2 
-        button2 = Button(self, text ="Page 2", 
-                            command = lambda : controller.show_frame(Page2)) 
-      
-        # putting the button in its place by  
-        # using grid 
-        button2.grid(row = 2, column = 1, padx = 10, pady = 10) 
-   
-   
-   
-   
-# third window frame page2 
-class Page2(Frame):  
-    def __init__(self, parent, controller): 
-        Frame.__init__(self, parent) 
-        label = Label(self, text ="Page 2") 
+
+class DriverPage(Frame):
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        label = Label(self, text ="Driver Page")
         label.grid(row = 0, column = 4, padx = 10, pady = 10) 
-   
-        # button to show frame 2 with text 
-        # layout2 
-        button1 = Button(self, text ="Page 1", 
-                            command = lambda : controller.show_frame(Page1)) 
-      
-        # putting the button in its place by  
-        # using grid 
-        button1.grid(row = 1, column = 1, padx = 10, pady = 10) 
-   
-        # button to show frame 3 with text 
-        # layout3 
-        button2 = Button(self, text ="Startpage", 
-                            command = lambda : controller.show_frame(StartPage)) 
-      
-        # putting the button in its place by 
-        # using grid 
-        button2.grid(row = 2, column = 1, padx = 10, pady = 10) 
-   
+
+class VehicleInormationPage(Frame):
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        label = Label(self, text ="Vehicle Inormation Page")
+        label.grid(row = 0, column = 4, padx = 10, pady = 10) 
+
+class TransactionHistoryPage(Frame):
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        label = Label(self, text ="Transaction History Page")
+        label.grid(row = 0, column = 4, padx = 10, pady = 10) 
+
+class DebugPage(Frame):
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        label = Label(self, text ="Debug Page")
+        label.grid(row = 0, column = 4, padx = 10, pady = 10) 
+
+class MenuSection(Listbox):  
+    def __init__(self, parent, controller): 
+        Listbox.__init__(self, parent,highlightthickness = '3', selectmode = BROWSE)
+        self.controller = controller
+
+        self.insert(0, 'General')
+        self.insert(1, 'Driver')
+        self.insert(2, 'Vehicle information')
+        self.insert(3, 'Transaction History')
+        self.insert(4, 'Debug')
+        
+        self.bind('<<ListboxSelect>>', self.show_frame)
+    
+    def show_frame(self, event):
+        for item in self.curselection():
+            if(item == 0):
+                self.controller.show_frame(GeneralPage)
+            elif(item == 1):
+                self.controller.show_frame(DriverPage)
+            elif(item == 2):
+                self.controller.show_frame(VehicleInormationPage)
+            elif(item == 3):
+                self.controller.show_frame(TransactionHistoryPage)
+            elif(item == 4):
+                self.controller.show_frame(DebugPage)   
+
    
 # Driver Code
 
 
 root=car_station_app()
-root.attributes("-fullscreen", True)
-root_height = root.winfo_height()
 
-root.columnconfigure(0, weight=45)
-root.columnconfigure(1, weight=55)
-root.rowconfigure(0, weight=10)
-root.rowconfigure(1, weight=1)
-
-menu = Listbox(root, highlightthickness = '3', selectmode = BROWSE)
-
-btn = Button(root, text = "Print width")
-
-menu.insert(1, 'General')
-menu.insert(2, 'Driver')
-menu.insert(3, 'Vehicle information')
-menu.insert(4, 'Transaction History')
-menu.insert(5, 'Debug')
-
-menu.grid(sticky="nsew", column = 0, row = 0)
 
 root.mainloop() 
